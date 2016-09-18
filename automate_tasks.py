8adff18f9a4b6dd7034b85716b8b75d956204035
Antonio Aguilar <juanantonioaguilar@gmail.com>
2016-09-18 16:47:22 +0200

    automate setting up environment

--- automate_tasks.py
+++ automate_tasks.py
@@ -1,39 +1,42 @@
 import os
-from subprocess import call
-
+from subprocess import run
+#setting up environment
+command = ['bash','-c','source ./environment.sh']
+res = run(command)
+if res.returncode:
+  raise Exception('set up environment failed!')
 github_user = os.environ['GITHUB_USER']
 github_passwd = os.environ['GITHUB_PASSWORD']
 project_name = os.environ['PROJECT_NAME']
-
 #project name
 print('project name ',project_name)
 #force install package
 print('installing package...')
-res = call(['pip','install','--upgrade','--force-reinstall','--no-deps','.'])
-if res:
+res = run(['pip','install','--upgrade','--force-reinstall','--no-deps','.'])
+print('pip install res',res)
+if res.returncode:
   raise Exception('pip failed!')
-
-#run test
+#run tests
 print('running tests...')
-res = call(['python', './tests/test_pieces.py'])
-if res:
+res = run(['python', './tests/test_pieces.py'])
+if res.returncode:
   raise Exception('test not found!')
 #git add
 print('git add...')
-call(['git', 'add', '.'])
+run(['git', 'add', '.'])
 #git commit
 print('git commit...')
 message = input('git commit message (blank to cancel): ') or 'n'
-
 if message != 'n':
-  call(['git', 'commit', '-m', message])
+  res = run(['git', 'commit', '-m', message])
+  if res.returncode:
+    raise Exception('git commit failed!')
 else:
   print('git commit skipped.')  
-
 #git push origin  
-print('git push origin master...')
 push_ok = input('Do you want to push changes to remote? [y/N]: ') or 'n'
+print('git push origin master...')
 if push_ok != 'n':
-  call(['git','push','https://{0}:{1}@github.com/{0}/{2}'.format(github_user,github_passwd,project_name),'master'])
+  run(['git','push','https://{0}:{1}@github.com/{0}/{2}'.format(github_user,github_passwd,project_name),'master'])
 else:
   print('git push skipped.')  
